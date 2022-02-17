dc2effdec3538abfaa98837456f4ed68862607f1
ryan <doanryan10@gmail.com>
2022-02-17 13:25:14 -0500

    open_or_senior - refactor with tuple unpacking

--- open_or_senior.py
+++ open_or_senior.py
@@ -13,7 +13,10 @@ def open_or_senior(data: List[Set]):
     # loop through the list of sets
     # first check if member is 55 or older & is hadicap greater then 7, if true reuturn "senior"
     # else return "open"
-    members = ["senior" if i[0] >= 55 and i[1] > 7 else "open" for i in data]
+    # members = ["senior" if i[0] >= 55 and i[1] > 7 else "open" for i in data]
+    members = [
+        "senior" if age >= 55 and handicap > 7 else "open" for (age, handicap) in data
+    ]
 
     print(members)
 
