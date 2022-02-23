35064567cba94f1d112e092af6e1f1d0646a9cfa
ShadFyt <doanryan10@gmail.com>
2022-02-22 22:20:42 -0600

    refactor list comp into genertor

--- digital_root.py
+++ digital_root.py
@@ -2,18 +2,13 @@ def digital_root(n):
     # first convert the number into a string
     # loop through the number(str) and append it to a list then convert it back into an int
     # sum the list of number
-    sum_num = sum([int(n) for n in str(n)])
-    if sum_num < 10:
-        # return sum_num if less then 10
-        return sum_num
-    else:
-        # rerun the function till number is less then 10
-        return digital_root(n=sum_num)
+    sum_num = sum(int(n) for n in str(n))
+    return sum_num if sum_num < 10 else digital_root(n=sum_num)
 
 
 def digital_root_one_liner(n):
     # same as above but a one liner
-    return n if n < 10 else digital_root_one_liner(sum([int(n) for n in str(n)]))
+    return n if n < 10 else digital_root_one_liner(sum(int(n) for n in str(n)))
 
 
 print(digital_root_one_liner(16))
